7e889b532d76351fd36008a2ab31f083b3079160
Dieter von Holten <info@dvholten.de>
2023-08-13 20:41:08 +0200

    more tests

--- src/main/java/org/example/SalesProductDTO.java
+++ src/main/java/org/example/SalesProductDTO.java
@@ -1,13 +1,34 @@
+//
+//  History:
+//  14-nov-22, dvh  JavaDoc: comment width of field 'bemerkung'
+//  17-sep-19, dvh  suppress warning for weaker access-level
+//  07-sep-18, dvh  patch the EtOH-MassKonz when not provided
+//  07-sep-19, dvh  annotations
+//  21-may-19, dvh  support new fields for PIDX-records
+//  19-aug-16, dvh  truncate the export-DtTm, drop the milliSecs
+//  16-aug-16, dvh  support etoh-data in HBLIF
+//  09-aug-16, dvh  support referenzNr in HBLIEF
+//  29-feb-16, dvh  use specific format for date/time-fields
+//  04-dec-15, dvh  new PRODUKTBESTAND for baseProduct
+//  26-nov-15, dvh  new for integra-interface
+//  27-nov-15, dvh  support einlagererNr
+//
 package org.example;
 
-import java.io.*;
+import java.io.Serial;
+import java.io.Serializable;
+import java.text.SimpleDateFormat;
 
-public class SalesProductDTO
-implements Serializable {
+import org.jetbrains.annotations.*;
+
+public final class SalesProductDTO
+        implements Serializable {
 
     @Serial
     private final static long serialVersionUID = 2023_08_13_1655L;
 
+
+    static final SimpleDateFormat dtTmFmt = new SimpleDateFormat( "yyMMdd HH:mm" );
 
     private int rowId = -1;
 
@@ -23,9 +44,10 @@ implements Serializable {
      */
     private String unStoffNr;
 
+    int zaehlerNr = 0;
     private RecordStatus recStatus = new RecordStatus();
 
-    public SalesProductDTO(){
+    public SalesProductDTO() {
 
     }
 
@@ -58,6 +80,13 @@ implements Serializable {
     }
 
     public void setUnStoffNr( String unStoffNr ) {
+
+        try {
+            int i = Integer.parseInt( unStoffNr );
+        } catch (NumberFormatException nfExc) {
+            throw new IllegalArgumentException( "must be numeric" );
+        }
+
         this.unStoffNr = unStoffNr;
     }
 
@@ -67,5 +96,39 @@ implements Serializable {
 
     public void setRecStatus( RecordStatus recStatus ) {
         this.recStatus = recStatus;
+    }
+
+
+    @Contract(pure = true)
+    public int getZaehlerNr() {
+        return zaehlerNr;
+    }
+
+    public void setZaehlerNr( int zaehlerNr ) {
+        this.zaehlerNr = zaehlerNr;
+    }
+
+    public void setZaehlerNr( String zlrNr ){
+
+        try {
+            if ( zlrNr.isEmpty() ) {
+                zlrNr = "00";
+            }
+            zaehlerNr = Integer.parseInt( zlrNr );
+        } catch ( Exception ex ){
+            System.err.println("*** error i ZlrNr: " + zlrNr );
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "SalesProductDTO{" +
+                "rowId=" + rowId +
+                ", productCode='" + productCode + '\'' +
+                ", productName='" + productName + '\'' +
+                ", unStoffNr='" + unStoffNr + '\'' +
+                ", zaehlerNr=" + zaehlerNr +
+                ", recStatus=" + recStatus +
+                '}';
     }
 }
--- src/test/java/org/example/SalesProductDTOTest.java
+++ src/test/java/org/example/SalesProductDTOTest.java
@@ -1,14 +1,18 @@
 package org.example;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertSame;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.mock;
 
 import java.time.LocalDate;
 import java.time.ZoneOffset;
 import java.util.Date;
 
 import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.Test;
 
 class SalesProductDTOTest {
 
@@ -19,20 +23,19 @@ class SalesProductDTOTest {
     }
 
     @BeforeEach
-    void beforeEachMethod(){
+    void beforeEachMethod() {
 
     }
 
     @AfterEach
-    void afterEachMethod(){
+    void afterEachMethod() {
 
     }
 
     @AfterAll
-    static void afterAllMethod(){
+    static void afterAllMethod() {
 
     }
-
 
 
     /**
@@ -89,6 +92,286 @@ class SalesProductDTOTest {
         // Assert
         assertEquals( -1, actualSalesProductDTO.getRowId() );
         assertTrue( actualSalesProductDTO.getRecStatus().flagIsBlocked );
+    }
+
+    /**
+     * Methods under test:
+     *
+     * <ul>
+     *   <li>default or parameterless constructor of {@link SalesProductDTO}
+     *   <li>{@link SalesProductDTO#setProductCode(String)}
+     *   <li>{@link SalesProductDTO#setProductName(String)}
+     *   <li>{@link SalesProductDTO#setRecStatus(RecordStatus)}
+     *   <li>{@link SalesProductDTO#setRowId(int)}
+     *   <li>{@link SalesProductDTO#getProductCode()}
+     *   <li>{@link SalesProductDTO#getProductName()}
+     *   <li>{@link SalesProductDTO#getRecStatus()}
+     *   <li>{@link SalesProductDTO#getRowId()}
+     *   <li>{@link SalesProductDTO#getUnStoffNr()}
+     * </ul>
+     */
+    @Test
+    void testConstructor3() {
+        // Arrange and Act
+        SalesProductDTO actualSalesProductDTO = new SalesProductDTO();
+        String productCode = "Product Code";
+        actualSalesProductDTO.setProductCode( productCode );
+        String productName = "Product Name";
+        actualSalesProductDTO.setProductName( productName );
+        RecordStatus recStatus = new RecordStatus();
+        recStatus.setFlagIsBlocked( true );
+        recStatus
+                .setLastModification( Date.from( LocalDate.of( 1970, 1, 1 ).atStartOfDay().atZone( ZoneOffset.UTC ).toInstant() ) );
+        actualSalesProductDTO.setRecStatus( recStatus );
+        int rowId = 1;
+        actualSalesProductDTO.setRowId( rowId );
+
+        // Assert
+        assertEquals( "Product Code", actualSalesProductDTO.getProductCode() );
+        assertEquals( "Product Name", actualSalesProductDTO.getProductName() );
+        assertSame( recStatus, actualSalesProductDTO.getRecStatus() );
+        assertEquals( 1, actualSalesProductDTO.getRowId() );
+        assertNull( actualSalesProductDTO.getUnStoffNr() );
+    }
+
+    /**
+     * Methods under test:
+     *
+     * <ul>
+     *   <li>default or parameterless constructor of {@link SalesProductDTO}
+     *   <li>{@link SalesProductDTO#setProductCode(String)}
+     *   <li>{@link SalesProductDTO#setProductName(String)}
+     *   <li>{@link SalesProductDTO#setRecStatus(RecordStatus)}
+     *   <li>{@link SalesProductDTO#setRowId(int)}
+     *   <li>{@link SalesProductDTO#setZaehlerNr(int)}
+     *   <li>{@link SalesProductDTO#toString()}
+     *   <li>{@link SalesProductDTO#getProductCode()}
+     *   <li>{@link SalesProductDTO#getProductName()}
+     *   <li>{@link SalesProductDTO#getRecStatus()}
+     *   <li>{@link SalesProductDTO#getRowId()}
+     *   <li>{@link SalesProductDTO#getZaehlerNr()}
+     * </ul>
+     */
+    @Test
+    void testConstructor4() {
+        // Arrange and Act
+        SalesProductDTO actualSalesProductDTO = new SalesProductDTO();
+        String productCode = "Product Code";
+        actualSalesProductDTO.setProductCode( productCode );
+        String productName = "Product Name";
+        actualSalesProductDTO.setProductName( productName );
+        RecordStatus recStatus = new RecordStatus();
+        recStatus.setFlagIsBlocked( true );
+        recStatus
+                .setLastModification( Date.from( LocalDate.of( 1970, 1, 1 ).atStartOfDay().atZone( ZoneOffset.UTC ).toInstant() ) );
+        actualSalesProductDTO.setRecStatus( recStatus );
+        int rowId = 1;
+        actualSalesProductDTO.setRowId( rowId );
+        int zaehlerNr = 1;
+        actualSalesProductDTO.setZaehlerNr( zaehlerNr );
+        actualSalesProductDTO.toString();
+
+        // Assert that nothing has changed
+        assertEquals( "Product Code", actualSalesProductDTO.getProductCode() );
+        assertEquals( "Product Name", actualSalesProductDTO.getProductName() );
+        assertSame( recStatus, actualSalesProductDTO.getRecStatus() );
+        assertEquals( 1, actualSalesProductDTO.getRowId() );
+        assertEquals( 1, actualSalesProductDTO.getZaehlerNr() );
+    }
+
+    /**
+     * Method under test: default or parameterless constructor of {@link SalesProductDTO}
+     */
+    @Test
+    void testConstructor5() {
+        // Arrange and Act
+        SalesProductDTO actualSalesProductDTO = new SalesProductDTO();
+
+        // Assert
+        assertEquals( 0, actualSalesProductDTO.getZaehlerNr() );
+        assertEquals( -1, actualSalesProductDTO.getRowId() );
+        assertTrue( actualSalesProductDTO.getRecStatus().flagIsBlocked );
+    }
+
+    /**
+     * Method under test: {@link SalesProductDTO#setUnStoffNr(String)}
+     */
+    @Test
+    void testSetUnStoffNr() {
+        // Arrange
+        SalesProductDTO salesProductDTO = new SalesProductDTO();
+        String unStoffNr = "Un Stoff Nr";
+
+        // Act and Assert
+        assertThrows( IllegalArgumentException.class, () -> salesProductDTO.setUnStoffNr( unStoffNr ) );
+    }
+
+    /**
+     * Method under test: {@link SalesProductDTO#setUnStoffNr(String)}
+     */
+    @Test
+    void testSetUnStoffNr2() {
+        // Arrange
+        SalesProductDTO salesProductDTO = new SalesProductDTO();
+        String unStoffNr = "42";
+
+        // Act
+        salesProductDTO.setUnStoffNr( unStoffNr );
+
+        // Assert
+        assertEquals( "42", salesProductDTO.getUnStoffNr() );
+    }
+
+    /**
+     * Method under test: {@link SalesProductDTO#setUnStoffNr(String)}
+     */
+    @Test
+    void testSetUnStoffNr3() {
+        // Arrange
+        RecordStatus recStatus = new RecordStatus();
+        recStatus.setFlagIsBlocked( true );
+        recStatus.setLastModification( mock( java.sql.Date.class ) );
+
+        SalesProductDTO salesProductDTO = new SalesProductDTO();
+        salesProductDTO.setRecStatus( recStatus );
+        String unStoffNr = "42";
+
+        // Act
+        salesProductDTO.setUnStoffNr( unStoffNr );
+
+        // Assert
+        assertEquals( "42", salesProductDTO.getUnStoffNr() );
+    }
+
+    /**
+     * Method under test: {@link SalesProductDTO#setUnStoffNr(String)}
+     */
+    @Test
+    void testSetUnStoffNr4() {
+        // Arrange
+        RecordStatus recStatus = new RecordStatus();
+        recStatus.setFlagIsBlocked( true );
+        recStatus.setLastModification( mock( java.sql.Date.class ) );
+
+        SalesProductDTO salesProductDTO = new SalesProductDTO();
+        salesProductDTO.setRecStatus( recStatus );
+        String unStoffNr = "42";
+
+        // Act
+        salesProductDTO.setUnStoffNr( unStoffNr );
+
+        // Assert
+        assertEquals( "42", salesProductDTO.getUnStoffNr() );
+    }
+
+    /**
+     * Method under test: {@link SalesProductDTO#setUnStoffNr(String)}
+     */
+    @Test
+    void testSetUnStoffNr5() {
+        // Arrange
+        SalesProductDTO salesProductDTO = new SalesProductDTO();
+        String unStoffNr = "00";
+
+        // Act
+        salesProductDTO.setUnStoffNr( unStoffNr );
+
+        // Assert
+        assertEquals( "00", salesProductDTO.getUnStoffNr() );
+    }
+
+    /**
+     * Method under test: {@link SalesProductDTO#setUnStoffNr(String)}
+     */
+    @Test
+    void testSetUnStoffNr6() {
+        // Arrange
+        RecordStatus recStatus = new RecordStatus();
+        recStatus.setFlagIsBlocked( true );
+        recStatus.setLastModification( mock( java.sql.Date.class ) );
+
+        SalesProductDTO salesProductDTO = new SalesProductDTO();
+        salesProductDTO.setRecStatus( recStatus );
+        String unStoffNr = "00";
+
+        // Act
+        salesProductDTO.setUnStoffNr( unStoffNr );
+
+        // Assert
+        assertEquals( "00", salesProductDTO.getUnStoffNr() );
+    }
+
+    /**
+     * Method under test: {@link SalesProductDTO#setZaehlerNr(String)}
+     */
+    @Test
+    void testSetZaehlerNr() {
+        // Arrange
+        SalesProductDTO salesProductDTO = new SalesProductDTO();
+        String zlrNr = "Zlr Nr";
+
+        // Act
+        salesProductDTO.setZaehlerNr( zlrNr );
+
+        // Assert that nothing has changed
+        assertEquals( 0, salesProductDTO.getZaehlerNr() );
+        assertEquals( -1, salesProductDTO.getRowId() );
+        assertTrue( salesProductDTO.getRecStatus().flagIsBlocked );
+    }
+
+    /**
+     * Method under test: {@link SalesProductDTO#setZaehlerNr(String)}
+     */
+    @Test
+    void testSetZaehlerNr2() {
+        // Arrange
+        SalesProductDTO salesProductDTO = new SalesProductDTO();
+        String zlrNr = "00";
+
+        // Act
+        salesProductDTO.setZaehlerNr( zlrNr );
+
+        // Assert
+        assertEquals( 0, salesProductDTO.getZaehlerNr() );
+    }
+
+    /**
+     * Method under test: {@link SalesProductDTO#setZaehlerNr(String)}
+     */
+    @Test
+    void testSetZaehlerNr3() {
+        // Arrange
+        SalesProductDTO salesProductDTO = new SalesProductDTO();
+        String zlrNr = "";
+
+        // Act
+        salesProductDTO.setZaehlerNr( zlrNr );
+
+        // Assert
+        assertEquals( 0, salesProductDTO.getZaehlerNr() );
+    }
+
+    /**
+     * Method under test: {@link SalesProductDTO#setZaehlerNr(String)}
+     */
+    @Test
+    void testSetZaehlerNr4() {
+        // Arrange
+        RecordStatus recStatus = new RecordStatus();
+        recStatus.setFlagIsBlocked( true );
+        recStatus.setLastModification( mock( java.sql.Date.class ) );
+
+        SalesProductDTO salesProductDTO = new SalesProductDTO();
+        salesProductDTO.setRecStatus( recStatus );
+        String zlrNr = "Zlr Nr";
+
+        // Act
+        salesProductDTO.setZaehlerNr( zlrNr );
+
+        // Assert that nothing has changed
+        assertEquals( 0, salesProductDTO.getZaehlerNr() );
+        assertEquals( -1, salesProductDTO.getRowId() );
+        assertTrue( salesProductDTO.getRecStatus().flagIsBlocked );
     }
 }
 
